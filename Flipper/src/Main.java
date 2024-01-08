88b3802312dd5f5f302422d199692c5d2c63cbb0
MGroegerAT <118934929+MGroegerAT@users.noreply.github.com>
2024-01-08 14:50:44 +0100

    files & file structure

--- Flipper/src/Main.java
+++ Flipper/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Instantiate and use the pinball machine
+        PinballMachine pinballMachine = new PinballMachine();
+        pinballMachine.insertCoin();
+        pinballMachine.pressStartButton();
     }
 }
